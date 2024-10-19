c79b5376e56cf9c112ff5972296e458bb0224b43
Amy Hwang <131427949+hwangsamy1@users.noreply.github.com>
2024-10-19 10:28:39 -0700

    Python file

---
+++ Project1/Project1/mymain.py
@@ -0,0 +1,16 @@
+
+# Step 0: load necessary Python packages
+import numpy as np
+import pandas as pd
+import scipy as sp
+import sklearn
+
+
+# Step 1: Preprocess the training data, then fit the two models.
+data = pd.read_csv("train.csv")
+
+# Step 2: Preprocess test data, then save predictions into two files:
+# mysubmission1.txt and mysubmission2.txt
+
+
+
